57d6cbb55e2b422c4c3f6c3285d4d870eace011e
MaxM <lynchnost@gmail.com>
2016-10-11 16:35:33 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,158 @@
+'use strict';
+
+/**
+ * Components and links
+ */
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  watch = require('gulp-watch'),
+  autoprefixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
+  // sourcemaps = require('gulp-sourcemaps'),
+  cmq = require('gulp-combine-mq'),
+  cssnano = require('gulp-cssnano'),
+  rename = require('gulp-rename'),
+  rimraf = require('rimraf'),
+  pngquant = require('imagemin-pngquant'),
+  imagemin = require('gulp-imagemin'),
+  htmlmin = require('gulp-htmlmin'),
+  
+  // BrowserSync
+  browserSync = require("browser-sync"),
+  reload = browserSync.reload;
+
+var path = {
+  build: {
+    html: 'build/',
+    js: 'build/js/',
+    css: 'build/css/',
+    img: 'build/img/',
+    sprite: 'build/img/sprite/',
+    fonts: 'build/fonts/'
+  },
+  src: {
+    html: 'src/*.html',
+    js: 'src/js/main.js',
+    styles: 'src/styles/main.scss',
+    img: 'src/img/**/*.*',
+    sprite: 'src/img/sprite/input/*.png',
+    fonts: 'src/fonts/**/*.*'
+  },
+  watch: {
+    html: 'src/**/*.html',
+    js: 'src/js/**/*.js',
+    styles: 'src/styles/**/*.scss',
+    img: 'src/img/**/*.*',
+    fonts: 'src/fonts/**/*.*'
+  },
+  clean: './build'
+};
+
+var config = {
+  server: {
+      baseDir: './build'
+  },
+  tunnel: false,
+  host: 'localhost',
+  port: 3000,
+  logPrefix: 'bratishka'
+};
+
+
+
+/**
+ * Gulp tasks
+ */
+gulp.task('clean', function (cb) {
+  rimraf(path.clean, cb);
+});
+
+
+gulp.task('html:build', function () {
+  gulp.src(path.src.html)
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(path.build.html))
+    .pipe(reload({stream: true}));
+});
+
+
+gulp.task('css:build', function () {
+  gulp.src(path.src.styles)
+    // .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions']
+      }))
+    .pipe(cmq())
+    .pipe(cssnano())
+    // .pipe(sourcemaps.write(path.build.css + '/sourcemaps'))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest(path.build.css))
+    .pipe(browserSync.stream());
+});
+
+
+gulp.task('js:build', function () {
+  gulp.src(path.src.js)
+    // .pipe(sourcemaps.init())
+    .pipe(uglify())
+    // .pipe(sourcemaps.write(path.build.css + '/sourcemaps'))
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest(path.build.js))
+    .pipe(reload({stream: true}));
+});
+
+
+gulp.task('img:build', function () {
+  gulp.src(path.src.img)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()],
+      interlaced: true
+    }))
+    .pipe(gulp.dest(path.build.img))
+    .pipe(browserSync.stream());
+});
+
+
+gulp.task('fonts:build', function() {
+  gulp.src(path.src.fonts)
+    .pipe(gulp.dest(path.build.fonts))
+});
+
+
+gulp.task('build', [
+  'html:build',
+  'css:build',
+  'js:build',
+  'fonts:build',
+  'img:build'
+]);
+
+
+gulp.task('webserver', function () {
+  browserSync(config);
+});
+
+
+gulp.task('watch', function() {
+  watch([path.watch.html], function(event, cb) {
+    gulp.start('html:build');
+  });
+  watch([path.watch.styles], function(event, cb) {
+    gulp.start('css:build');
+  });
+  watch([path.watch.js], function(event, cb) {
+    gulp.start('js:build');
+  });
+  watch([path.watch.img], function(event, cb) {
+    gulp.start('img:build');
+  });
+  watch([path.watch.fonts], function(event, cb) {
+    gulp.start('fonts:build');
+  });
+});
+
+
+gulp.task('default', ['build', 'webserver', 'watch']);
---
+++ readme.md
@@ -0,0 +1,9 @@
+This is a course project for [AngularJS](http://learn.javascript.ru/courses/angular) course, run by [javascript.ru](http://javascript.ru) team.
+
+*Dates:* 26.09.2016—01.11.2016.
+
+## How to deploy
+
+1. Clone the project from Github.
+2. Install the necessary Node modules via NPM (run `npm install` in your terminal while in the project root directory). Note: you also need to have Node.js installed on your machine.
+3. Run `gulp` command in the terminal window.
