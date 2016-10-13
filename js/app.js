5fc8dd86f1562dd01abb2fe1760ca786445879cc
MaxM <lynchnost@gmail.com>
2016-10-13 17:32:51 +0300

    13.10.2016 17:32

---
+++ js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+/**
+ * Paths must be relative to the COMPILED .js file, but with compensation to
+ * gh-pages structure: `domain/project/index.html`
+ *
+ * !!!TODO: cope with minify/uglify issues!!!
+ */
+
+var app = angular.module('myMailApp', []);
+
+app.service('MailService', function($http) {
+  this.getLetters = function() {
+    return $http.get('http://test-api.javascript.ru/v1/maxm/letters').
+        then(
+          function(response) {
+            return response.data; //success
+        },
+          function(response) {
+            console.log('Houston we\'ve got problems:' + response.status + ', ' + response.statusText);
+          });
+  }
+});
+
+app.component('mailbox', {
+  templateUrl: 'templates/letters.tpl.html',
+    controller: ['MailService', function(MailService) {
+      var self = this;
+      MailService.getLetters()
+        .then(function(letters) {
+          return self.letters = letters;
+        });
+    }]
+});
